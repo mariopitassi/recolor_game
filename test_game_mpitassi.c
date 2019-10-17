2b82d5dcaf2017a5016374b30aadcacddd1a08f0
Mario PITASSI <mario.pitassi@etu.u-bordeaux.fr>
2019-10-17 13:51:34 +0200

    bug: test_game_mpitassi et Makefile

--- test_game_mpitassi.c
+++ test_game_mpitassi.c
@@ -1,35 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "game.h"
 
-/* ****** TEST 1 ***** */
+/* ****** TEST GAME_NEW ***** */
+
+bool test_game_new(game* g, color* tab, uint coup) {
+
+    if(g == NULL) {
+        fprintf(stderr, "Table de la structure game non initialisé.");
+        return false;
+    }
+
+    for (int i = 0; i <= SIZE - 1; i++) {
+        for (int j = 0; j <= SIZE - 1; j++) {
+            if (game_cell_current_color(*g, j, i) != tab[12*i+j]) {
+                fprintf(stderr, "Les cellules ne sont pas initialisées avec le tableau.");
+                return false;
+            }
+        }
+    }
+
+    if(game_nb_moves_max(g) != coup) {
+        return false;
+    }
+
+        return true;
+}
+
 
 int main(int argc, char *argv[]) {
-    if (argc == 1)
-        usage(argc, argv);
+
+    // Initialisation d'un jeu.
+
+    color tab[] = {
+        0, 0, 0, 2, 0, 2, 1, 0, 1, 0, 3, 0,
+        0, 3, 3, 1, 1, 1, 1, 3, 2, 0, 1, 0,
+        1, 0, 1, 2, 3, 2, 3, 2, 0, 3, 3, 2,
+        2, 3, 1, 0, 3, 2, 1, 1, 1, 2, 2, 0,
+        2, 1, 2, 3, 3, 3, 3, 2, 0, 1, 0, 0,
+        0, 3, 3, 0, 1, 1, 2, 3, 3, 2, 1, 3,
+        1, 1, 2, 2, 2, 0, 0, 1, 3, 1, 1, 2,
+        1, 3, 1, 3, 1, 0, 1, 0, 1, 3, 3, 3,
+        0, 3, 0, 1, 0, 0, 2, 1, 1, 1, 3, 0,
+        1, 3, 1, 0, 0, 0, 3, 2, 3, 1, 0, 0,
+        1, 3, 3, 1, 1, 2, 2, 3, 2, 0, 0, 2,
+        2, 0, 2, 3, 0, 1, 1, 1, 2, 3, 0, 1};
+
+    uint coups_max = 12;
+
+    game game = game_new(tab, coups_max);
+
 
     // start test
     fprintf(stderr, "=> Start test mpitassi \"%s\"\n", argv[1]);
     bool ok = false;
-    if (strcmp("init_free", argv[1]) == 0)
-        ok = test_init_free();
-    else if (strcmp("push", argv[1]) == 0)
-        ok = test_push(100);
-    else
-    {
+
+    if (strcmp("game_new", argv[1]) == 0)
+        ok = test_game_new(game, tab, coups_max);
+    else {
         fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
     // print test result
-    if (ok)
-    {
+    if (ok){
         fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
         return EXIT_SUCCESS;
-    }
-    else
-    {
+    }else {
         fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
         return EXIT_FAILURE;
     }
