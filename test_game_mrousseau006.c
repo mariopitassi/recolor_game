96a8f1dd4b34fe7ef8f85bedf2ce893f92fbfdd6
Rousseau <mattias.rousseau@etu.u-bordeaux.fr>
2019-10-17 13:54:53 +0200

    add : modif du fichier test Mattias

--- test_game_mrousseau006.c
+++ test_game_mrousseau006.c
@@ -1,1 +1,31 @@
-qffsdxv
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "game.h"
+
+/* test game new empty */
+
+
+bool test_gameNewEmpty (game* g){
+
+    if (g == NULL){
+        fprintf(stderr,"le tableau du jeu n'est pas intialisé");
+        return false;
+    }
+
+    for(int i=0; i<=SIZE-1; i++) {
+        for(int j=0; j<=SIZE-1; j++) {
+
+            if (game_cell_current_color (*g, j, i) != 0){
+                fprintf(stderr,"le tableau n'est pas initialisé à 0");
+                return false;
+            }
+        }
+    }
+
+    if()
+}
+
+
