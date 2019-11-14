05cf41a85017d7ddae0ae2038650b14232a22f9e
Rousseau <mattias.rousseau@etu.u-bordeaux.fr>
2019-11-14 10:27:43 +0100

    ajout des premières fonctions Mattias

--- game.c
+++ game.c
@@ -19,3 +19,35 @@ void error(char[100] err_mess) {
     exit(EXIT_FAILURE);
 }
 
+void game_restart(game g){
+    
+    if(g == NULL){
+        error(Pointeur est nul);
+    }
+
+    for(int i = 0; i < g->size*g->size ; i++){
+
+        g->tab_cur[i] = g->tab_init[i];
+    }
+
+    g->move_cur = 0;
+}
+
+uint game_nb_moves_max(game g){
+
+    uint nb_moves_max = g->move_max;
+
+    return nb_moves_max;
+}
+
+void game_delete (game g){
+
+    if(g == NULL){
+        error(Pointeur est nul);
+    }
+
+    else {
+        free(g->tab_cur);
+        free(g);
+    }
+}
