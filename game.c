3f63eb8429d06d706769849c88b2b725cd02a738
Farouk <farouk.akhoun@etu.u-bordeaux.fr>
2019-11-16 12:15:17 +0100

    Débogage de mes fonctions

--- game.c
+++ game.c
@@ -193,16 +193,9 @@ void game_delete (game g){
 
 /* ************* Author : Farouk *********** */ 
 
-/**
- * @brief Creates a new game and initializes it in one call.
- * @param cells 1D array describing the color of each cell of the game. The storage is row by row
- * @param nb_max_moves the value of the maximum number of moves
- * @return the created game
- * @pre @p cells is an initialized array of size SIZE*SIZE.
- * @pre @p nb_max_moves > 0
- **/
+
 game game_new(color *cells, uint nb_moves_max){                     
-    game g = malloc(sizeof(game));
+    game g = malloc(sizeof(struct game_s));
     if (g == NULL) 
         error("g allocation went wrong\n");
     g->tab_init = malloc(SIZE*SIZE*sizeof(color));
@@ -223,14 +216,9 @@ game game_new(color *cells, uint nb_moves_max){
     return g;
 }
 
-/**
- * @brief Creates an empty game.
- * @details Creates an empty game having SIZE rows and SIZE columns. All the cells will have the default color
- * (whose value is RED). The maximum number of moves is set to 0.
- * @return The created game
- **/
+
 game game_new_empty(){
-    game g = malloc(sizeof(game));
+    game g = malloc(sizeof(struct game_s));
     if (g == NULL)
         error("g allocation went wrong\n");
     g->tab_init = calloc(SIZE*SIZE, sizeof(color));
@@ -245,16 +233,7 @@ game game_new_empty(){
     return g;
 }
 
-/**
- * @brief Gets the color of the cell located at given coordinates.
- * @param g the game
- * @param x the first coordinate of the cell
- * @param y the second coordinate of the cell
- * @return the color of the cell
- * @pre @p g is a valid pointer toward a cgame structure
- * @pre @p x < SIZE
- * @pre @p y < SIZE
- **/
+
 color game_cell_current_color(cgame g, uint x, uint y){
     if (g == NULL)
         error("g is not a valid pointer");
