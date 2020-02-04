3e4a027a521bad6e08ab1b3345510e1340e5ea5e
Mario PITASSI <mario.pitassi@etu.u-bordeaux.fr>
2020-02-04 21:23:09 +0100

    Update .gitignore + MAJ helper.sh

--- helper.sh
+++ helper.sh
@@ -1,16 +1,15 @@
 #bin/sh
 
-me=`basename "$0"`
-
 if [ $# = 0 ]; then 
     echo "$(tput setaf 3)Usage : helper.sh <command>"
-    echo "$(tput setaf 7)where command is one of build, test, clear, coverage\n"
-    echo "build - Create a build dir and copy executables in run dir"
-    echo "test - Run all the test of the program"
-    echo "clear - Clean the projet (del: /build)"
-    echo "coverage - Run coverage test"
+    echo "$(tput setaf 7)where command is one of build, test, clear, coverage and switch\n"
+    echo "$(tput bold) build$(tput sgr0) - Create a build dir and copy executables in run dir"
+    echo "$(tput bold) test$(tput sgr0)  - Run all the test of the program"
+    echo "$(tput bold) clear$(tput sgr0) - Clean the projet (del: /build)"
+    echo "$(tput bold) coverage$(tput sgr0) - Run coverage test"
+    echo "$(tput bold) switch$(tput sgr0) <V1|V2> - Change version of the project"
 else
-    if [ $1 = "build" ]
+    if [ "$1" = "build" ]
     then
         if [ ! -d "./build" ]; then
             mkdir build
@@ -19,15 +18,15 @@ else
             (cd build && make)
         fi
 
-    elif [ $1 = "test" ]
+    elif [ "$1" = "test" ]
     then
-        if [ ! -d "./build"]; then 
-            me build
+        if [ ! -d "./build" ]; then 
+            ./helper.sh build
         fi
 
         (cd build && ctest .)
 
-    elif [ $1 = "clear" ] || [ $1 = "clean" ]
+    elif [ "$1" = "clear" ] || [ "$1" = "clean" ]
     then
         if [ -d "./build" ]; then
             rm -r ./build
@@ -35,14 +34,24 @@ else
 
         echo "$(tput setaf 2)Clean done !"
         
-    elif [ $1 = "coverage" ]
+    elif [ "$1" = "coverage" ]
     then
         if [ ! -d "./build" ]; then 
             ./helper.sh build
         fi
 
         (cd build && ctest && make ExperimentalCoverage)
+
+    elif [ "$1" = "switch" ]
+    then
+        if [ "$2" = "v1" ] || [ "$2" = "V1" ]; then 
+            git checkout v1
+        elif [ "$2" = "v2" ] || [ "$2" = "v2" ]; then
+            git checkout master
+        else
+            echo "Usage : ./helper.sh switch <v1|v2>"
+        fi
     else
-        me
+        ./helper.sh
     fi
 fi
